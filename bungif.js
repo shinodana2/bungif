e94c0b2b2987bf15bf097a9eba82fa7f0ea77a50
gout <gout@DESKTOP-QFTTLU5>
2019-08-05 06:32:49 +0900

    v2

--- bungif.js
+++ bungif.js
@@ -68,7 +68,6 @@ function init() {
 
 	onEditingText();
 	prepareAnimation();
-	onColorBgRed();
 }
 
 function setMainCanvas() {
@@ -611,50 +610,12 @@ function rgb2hsv(rgb) {
 	return [ h, s, v ];
 }
 
-function hsv2rgb(hsv) {
-	var h = hsv[0] / 60;
-	var s = hsv[1];
-	var v = hsv[2];
-	if (s == 0)
-		return [ v * 255, v * 255, v * 255 ];
-
-	var rgb;
-	var i = parseInt(h);
-	var f = h - i;
-	var v1 = v * (1 - s);
-	var v2 = v * (1 - s * f);
-	var v3 = v * (1 - s * (1 - f));
-
-	switch (i) {
-	case 0:
-	case 6:
-		rgb = [ v, v3, v1 ];
-		break;
-	case 1:
-		rgb = [ v2, v, v1 ];
-		break;
-	case 2:
-		rgb = [ v1, v, v3 ];
-		break;
-	case 3:
-		rgb = [ v1, v2, v ];
-		break;
-	case 4:
-		rgb = [ v3, v1, v ];
-		break;
-	case 5:
-		rgb = [ v, v1, v2 ];
-		break;
-	}
-
-	return rgb.map(function(value) {
-		return value * 255;
-	});
-}
-
-function onRangeChange(h) {
-	var v = hsv2rgb([ h, 0.9, 0.7 ]);
-	colorBgBase = "rgb(" + v[0] + "," + v[1] + "," + v[2] + ")";
+function onRangeChange() {
+	var r = document.getElementById('value_red').value;
+	var g = document.getElementById('value_green').value;
+	var b = document.getElementById('value_blue').value;
+	
+	colorBgBase = "rgb(" + r + "," + g + "," + b + ")";
 
 	drawStaticPart();
 }
